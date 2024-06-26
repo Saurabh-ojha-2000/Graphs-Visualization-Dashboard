341d6e4256105af8fd9b47148c0a4fe1e1395271
Saurabh Ojha <107386248+Saurabh-ojha-2000@users.noreply.github.com>
2024-06-26 16:09:27 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
         <div className="header">
           <h1 style={{ padding: "10px" }} className="header-topic">
-            Blackcoffer Visualization Dashboard
+            Visualization Dashboard
           </h1>
         </div>
     </>
